76fa94dc8f7101a87e0da801585056401511f342
Max Rusakovič <maxrusakovic@gmail.com>
2021-11-21 20:19:49 +0100

    [12] Added PackCardRecipeStyles

--- components/PackCardRecipe.tsx
+++ components/PackCardRecipe.tsx
@@ -1,7 +1,7 @@
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import React from 'react'
-import { View, Image, Text, Pressable } from 'react-native'
+import { View, Image, Text, Pressable, StyleSheet } from 'react-native'
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import { PackWithKeys, TransformedRecipe } from '../features/recipes/types'
 import { colour, radius } from '../styles'
@@ -30,47 +30,51 @@ const PackCardRecipe: React.FunctionComponent<PackCardRecipeProps> = ({
     <Pressable
       key={id}
       onPress={onPressRecipeHandler}
-      style={{
-        marginHorizontal: 15,
-        marginVertical: 10,
-        height: hp(20),
-        backgroundColor: colour.white.fullWhite,
-        elevation: 5,
-        shadowOffset: {
-          height: 1,
-          width: 0,
-        },
-        shadowColor: colour.black.fullBlack,
-        shadowOpacity: 0.2,
-        shadowRadius: 7,
-        borderRadius: radius.sm,
-      }}
+      style={PackCardRecipeStyles.mainShadowWrapper}
     >
-      <View
-        style={{
-          height: '80%',
-          overflow: 'hidden',
-          borderTopStartRadius: radius.sm,
-          borderTopEndRadius: radius.sm,
-        }}
-      >
+      <View style={PackCardRecipeStyles.imageWrapper}>
         <Image
           resizeMode='cover'
           source={{ uri: image }}
-          style={{ height: '100%', width: '100%' }}
+          style={PackCardRecipeStyles.image}
         />
       </View>
-      <View
-        style={{
-          height: '20%',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+      <View style={PackCardRecipeStyles.titleWrapper}>
         <Text>{title}</Text>
       </View>
     </Pressable>
   )
 }
+
+const PackCardRecipeStyles = StyleSheet.create({
+  mainShadowWrapper: {
+    marginHorizontal: 15,
+    marginVertical: 10,
+    height: hp(20),
+    backgroundColor: colour.white.fullWhite,
+    elevation: 5,
+    shadowOffset: {
+      height: 1,
+      width: 0,
+    },
+    shadowColor: colour.black.fullBlack,
+    shadowOpacity: 0.2,
+    shadowRadius: 7,
+    borderRadius: radius.sm,
+  },
+
+  imageWrapper: {
+    height: '80%',
+    overflow: 'hidden',
+    borderTopStartRadius: radius.sm,
+    borderTopEndRadius: radius.sm,
+  },
+  image: { height: '100%', width: '100%' },
+  titleWrapper: {
+    height: '20%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
 
 export default PackCardRecipe
